d1b30c08bacb5f8d10667481083b84de8d490aa2
Nikola Milutinovic <n.milutinovic@levi9.com>
2019-11-22 18:53:08 +0100

    [remove:] Trimmed out annotations.

--- backend/src/main/java/com/levi9/hack9/reference2019/rest/SwitchboardController.java
+++ backend/src/main/java/com/levi9/hack9/reference2019/rest/SwitchboardController.java
@@ -4,17 +4,13 @@
 package com.levi9.hack9.reference2019.rest;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -39,9 +35,6 @@ public class SwitchboardController extends SwitchboardApiController {
 	}
 	
 	@Override
-    @RequestMapping(value = "/switchboard/price",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
     public ResponseEntity<Price> getPrice(
     		@NotNull @Valid @RequestParam(value = "number", required = true) String number,
     		@NotNull @Valid @RequestParam(value = "time", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime time) {
